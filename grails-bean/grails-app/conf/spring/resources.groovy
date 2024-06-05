def9b3eb9ada3fe49f6bd65973f4eb7a0de6a486
Robert Pastel <pastel@mtu.edu>
2024-06-05 12:46:48 -0400

    Minor

--- grails-bean/grails-app/conf/spring/resources.groovy
+++ grails-bean/grails-app/conf/spring/resources.groovy
@@ -2,7 +2,7 @@
 import grails.bean.MyBean
 
 /*
- * beans resigisters and instantiates beans.
+ * beans registers and instantiates beans.
  * See https://docs.grails.org/6.2.0/guide/spring.html for simple examples.
  * See https://stackoverflow.com/questions/22400078/inject-constructor-argument-spring-resource-file-with-grails-groovy
  * for how to inject constructor arguments.
